f28deca3e692227b1703206d0b30c49708f25358
David Felix <david.felix02@hotmail.com>
2020-09-14 00:46:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,21 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 function greet(hour_time) {
-  let arr_of_hour = hour_time.split("")
-  arr_of_hour.splice(2,1)
-  let arr_of_num = arr_of_hour.map(Number)
-  let num = arr_of_num.join("")
+  let arr_of_hour = hour_time.split(":")
+  hour =  arr_of_hour.join("")
  
-  if (num<1200){
+  if (hour<1200){
     return console.log("Good Morning")
   }
-  else if(num>1700){
+  else if(hour>1700){
     return console.log("Good Evening")
   }
   else {
     return console.log("Good Afternoon")
   }
-  return "hello"
 }
 
 function displayMessage() {
