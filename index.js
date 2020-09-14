394031880191b1f6bc34a2519a156252a1b926f9
David Felix <david.felix02@hotmail.com>
2020-09-14 00:53:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ function greet(timeString) {
 
 function displayMessage(the_string) {
   let element = getElementById("greeting");
-  element.innerHTML = the_string;
+  element.innerText = the_string;
 }
