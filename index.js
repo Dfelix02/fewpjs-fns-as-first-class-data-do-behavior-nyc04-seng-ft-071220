f72c7bec9a9e346b5fdf76ab78c39879fafe7b2c
David Felix <david.felix02@hotmail.com>
2020-09-14 00:37:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,16 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 function greet(hour_time) {
-  if (hour_time<1200){
+  let arr_of_hour = hour_time.split("")
+  arr_of_hour.splice(2,1)
+  let arr_of_num = arr_of_hour.map(Number)
+  num = arr_of_num.join("")
+ 
+  if (num<1200){
     return console.log("Good Morning")
   }
-  else if(hour_time>1700){
+  else if(num>1700){
     return console.log("Good Evening")
   }
   else {
     return console.log("Good Afternoon")
   }
 }
+
 function displayMessage() {
 
 }
